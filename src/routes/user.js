54e0e591e7536457798629fb29ee14d4eee2b69a
Alexis Tyler <xo@wvvw.me>
2017-07-02 02:09:50 +0930

    send token on signup

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,9 +2,11 @@ import {Router} from 'express';
 import Joi from 'joi';
 import HTTPError from 'http-errors';
 import dumbPasswords from 'dumb-passwords';
+import jwt from 'jsonwebtoken';
 import {isAuthenticated} from '../middleware';
 import {User} from '../models';
 import log from '../log';
+import config from '../config';
 
 const router = new Router();
 
@@ -46,16 +48,25 @@ router.post('/', (req, res, next) => {
                 return next(new HTTPError.InternalServerError(`User could not be saved.`));
             }
             // Just to be sure we remove the password here incase we fuck up somewhere.
-            const {username, email, roles, plan, tumblr, apiKey} = user;
-            return res.status(201).send({
-                user: {
-                    username,
-                    email,
-                    roles,
-                    plan,
-                    tumblr,
-                    apiKey
+            const {username, roles} = user;
+
+            jwt.sign({
+                username,
+                roles,
+                iat: Math.floor(Date.now() / 1000) - 5, // Set issue date 5 seconds ago
+                iss: 'https://api.twistly.xyz',
+                aud: 'https://api.twistly.xyz',
+                maxAge: 3600
+            }, config.get('jwt.secret'), {
+                expiresIn: 3600
+            }, (err, token) => {
+                if (err) {
+                    return next(err);
                 }
+                log.debug('Sending JWT.');
+                return res.status(201).send({
+                    token
+                });
             });
         });
     });
