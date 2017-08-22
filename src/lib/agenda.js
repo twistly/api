f27b2bea75ad7cc321600ac8baf3e2e0820f9cae
Alexis Tyler <xo@wvvw.me>
2017-08-23 01:49:07 +0930

    fix jobs running before agenda started

--- src/lib/agenda.js
+++ src/lib/agenda.js
@@ -5,6 +5,8 @@ import email from '../jobs/email';
 const uri = process.env.MONGO_URL || config.get('database.url');
 const agenda = new Agenda({db: {address: uri}});
 
-email(agenda);
+agenda.on('ready', () => {
+    email(agenda);
+});
 
 export default agenda;
