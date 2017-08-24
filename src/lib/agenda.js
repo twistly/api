f96ca86759ebe5531a8e2c0924eebac9d3f6dabb
Alexis Tyler <xo@wvvw.me>
2017-08-25 01:57:48 +0930

    allow agneda to start all jobs exported from jobs dir and autostart queue job

--- src/lib/agenda.js
+++ src/lib/agenda.js
@@ -1,15 +1,33 @@
 import Agenda from 'agenda';
 import d from 'debug';
 import config from '../config';
-import email from '../jobs/email';
+import jobs from '../jobs';
+import {Queue} from '../models';
+
+const ONE_MINUTE = 60;
+const ONE_HOUR = 60 * ONE_MINUTE;
+const ONE_DAY = 24 * ONE_HOUR;
 
 const debug = d('twistly:lib:agenda');
 const uri = process.env.MONGO_URL || config.get('database.url');
 const agenda = new Agenda({db: {address: uri}});
 
-agenda.on('ready', () => {
+agenda.on('ready', async () => {
     debug('agenda ready');
-    email(agenda);
+
+    Object.keys(jobs).forEach(job => jobs[job](agenda));
+
+    if (process.env.JOB_TYPES.indexOf('queue') !== -1) {
+        const queues = await Queue.find().select('_id internal owner').lean().exec();
+        queues.forEach(queue => {
+            const interval = ONE_DAY / queue.interval;
+
+            agenda.every(`${interval} seconds`, 'check queue', {
+                userId: queue.owner,
+                queueId: queue._id
+            });
+        });
+    }
 });
 
 export default agenda;
